50f9f97195f675920bbe412cd9d747f4f9b6fb3c
Natsusora-no-Perseus <89143712+Natsusora-no-Perseus@users.noreply.github.com>
2022-06-13 19:02:39 +0800

    Update JoystickBasics.cpp

--- src/JoystickBasics.cpp
+++ src/JoystickBasics.cpp
@@ -10,30 +10,29 @@
 
 JoystickBasics::JoystickBasics(uint8_t inputPin, uint16_t midPoint, bool invertInput)
 {
-  _inputPin = inputPin;
-  _invertInput = invertInput;
-  _midPoint = midPoint;
+	_inputPin = inputPin;
+	_invertInput = invertInput;
+	_midPoint = midPoint;
 }
 
 void JoystickBasics::calibrateInput()
 {
-  _calibrationValue = _midPoint - analogRead(_inputPin);
+	_calibrationValue = _midPoint - analogRead(_inputPin);
 }
 
 void JoystickBasics::setSensitivity(uint8_t sensitivityFactor)
 {
-  _sensitivityFactor = sensitivityFactor;
+	_sensitivityFactor = sensitivityFactor;
 }
 
 int16_t JoystickBasics::getPos()
 {
-  if (_invertInput == true)
-  {
-    return (_midPoint - analogRead(_inputPin) - _calibrationValue);
-  }
-  else
-  {
-    return (analogRead(_inputPin) - _midPoint + _calibrationValue);
-  }
+	if (_invertInput == true)
+	{
+		return (_midPoint - analogRead(_inputPin) - _calibrationValue);
+	}
+	else
+	{
+		return (analogRead(_inputPin) - _midPoint + _calibrationValue);
+	}
 }
-
