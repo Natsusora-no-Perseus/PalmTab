38b69ed2c3a7d516242ab90bf1fab59d1c87a27e
Natsusora-no-Perseus <89143712+Natsusora-no-Perseus@users.noreply.github.com>
2022-06-22 23:28:47 +0800

    Update CurveEditor.h

--- src/CurveEditor.h
+++ src/CurveEditor.h
@@ -29,12 +29,24 @@ private:
 		int8_t xPos;
 		int8_t yPos;
 	};
-
-	float scalingFactor;//Shrinks the distance between pairs of control points; adjusts "sharpness" of curve
+	
+	struct FloatNodePos
+	{
+		float xPos;
+		float yPos;
+	}
 
 	vector<NodePos> _nodesList(2);//Stores list of NodePos
 	vector<NodePos> _midpointsList(2);//Stores list of midpoints
 	vector<NodePos> _shiftedPointsList(2);//Stores list of shifted midpoints
+	vector<NodePos> _resultNodes(2);//Stores list of result NodePos
+	
+	
+	
+	float _scalingFactor;//Shrinks the distance between pairs of control points; adjusts "sharpness" of curve
+	uint8_t bezierSubIntv;//Number of subintervals that the bezier curve should have.
+	
+	
 
 	NodePos getMidpoint(uint8_t nodeIndex);//Find midpoint between this node and next node
 
@@ -45,6 +57,16 @@ private:
 	NodePos getShiftDist(uint8_t nodeIndex);//Find shift distance for this and next midpoint
 
 	NodePos updateShift();//Updates list shifted midpoints (also control points for bezier curve)
+
+	void setScalingFactor(float scalingFactor);//Should be set between 0~1. Larger = "sharper" curve. Suggested value is 0.6.
+	
+	void setBezierSubIntv(uint8_t inputSubIntv);
+	
+	FloatNodePos getTPoint(FloatNodePos point1, FloatNodePos point2, float tValue)//Gets point at (tValue)th of the line segment
+	
+	NodePos bezierRecursive(float tValue)//Gets the point on bezier curve at tValue.
+	
+	NodePos bezierList()//Places nodes in _resultNodes.
 };
 
 #endif
