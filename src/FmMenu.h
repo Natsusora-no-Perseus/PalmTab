2cb1088917bc6a84ca464d80d62311ec21362e7b
Natsusora-no-Perseus <89143712+Natsusora-no-Perseus@users.noreply.github.com>
2022-07-18 23:36:20 +0800

    Create FmMenu.h

---
+++ src/FmMenu.h
@@ -0,0 +1,37 @@
+/* FmMenu.h - A simple library for creating a tree menu structure.
+ * Used to support a simple GUI menu.
+ *
+ * Created by Natsusora-no-Perseus@GitHub.
+ * Date: 2022/07/18 (YYYY/MM/DD)
+ *
+ * License: GPLv3 or higher.
+ */
+
+#ifndef FmMenu_h
+#define FmMenu_h
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;//I do know it's wrong to do this in a library. Will remove it later.
+
+
+
+class FmMenu
+{
+public:
+
+
+
+private:
+
+
+
+};
+
+
+#endif
