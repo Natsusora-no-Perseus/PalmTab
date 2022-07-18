aa91ad5fe91c18951f51225c5c2273f2ab2cb22c
Natsusora-no-Perseus <89143712+Natsusora-no-Perseus@users.noreply.github.com>
2022-07-18 23:38:38 +0800

    Create FmMenu.cpp

---
+++ src/FmMenu.cpp
@@ -0,0 +1,11 @@
+/* FmMenu.cpp - A simple library for creating a tree menu structure.
+ * Used to support a simple GUI menu.
+ *
+ * Created by Natsusora-no-Perseus@GitHub.
+ * Date: 2022/07/18 (YYYY/MM/DD)
+ *
+ * License: GPLv3 or higher.
+ */
+
+#include "FmMenu.h"
+
